4ad37dbc802455cdf1e245f5498329fa3a6d5225
Shivam Verma <vermashivam527@gmail.com>
2021-06-08 00:35:33 +0530

    trying setting headers

--- index.js
+++ index.js
@@ -5,22 +5,31 @@ const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const app = express();
 
-const PORT = process.env.PORT || 8000;
-
-const { initializeDBConnection } = require("./db/db.config");
-
-initializeDBConnection();
-
-app.use(bodyParser.json());
-app.use(cors({ credentials: true, origin: "https://quizviz.netlify.app" }));
-app.use(cookieParser());
-
 const userRouter = require("./routes/user.route");
 const scoreRouter = require("./routes/score.route");
 const authRouter = require("./routes/auth.route");
 const questionRouter = require("./routes/question.route");
 const { errorHandler } = require("./middlewares/errorHandler.middleware");
 const { routeNotFound } = require("./middlewares/routeNotFound.middleware");
+
+const PORT = process.env.PORT || 8000;
+
+const appURL = "https://quizviz.netlify.app/";
+
+const { initializeDBConnection } = require("./db/db.config");
+
+initializeDBConnection();
+
+app.use(bodyParser.json());
+app.use(cors({ credentials: true, origin: appURL, metods: "GET, POST" }));
+app.use(cookieParser());
+
+app.all("*", function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", appURL);
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
 app.use("/api/user", userRouter);
 app.use("/api/score", scoreRouter);
