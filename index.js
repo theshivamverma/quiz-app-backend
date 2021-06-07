696f430f633f5c25034721e153a5d8df03dab1e5
Shivam Verma <vermashivam527@gmail.com>
2021-06-07 16:54:52 +0530

    index file changed

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const cors = require("cors")
-const cookieParser = require("cookie-parser")
-require("dotenv").config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 const app = express();
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
-const { initializeDBConnection } = require("./db/db.config")
+const { initializeDBConnection } = require("./db/db.config");
 
 initializeDBConnection();
 
-app.use(bodyParser.json())
-app.use(cors({ credentials: true, origin: "localhost:3000" }));
-app.use(cookieParser())
+app.use(bodyParser.json());
+app.use(cors({ credentials: true }));
+app.use(cookieParser());
 
-const userRouter  = require("./routes/user.route")
-const scoreRouter  = require("./routes/score.route")
-const authRouter  = require("./routes/auth.route")
-const questionRouter = require("./routes/question.route")
+const userRouter = require("./routes/user.route");
+const scoreRouter = require("./routes/score.route");
+const authRouter = require("./routes/auth.route");
+const questionRouter = require("./routes/question.route");
 const { errorHandler } = require("./middlewares/errorHandler.middleware");
 const { routeNotFound } = require("./middlewares/routeNotFound.middleware");
-
 
 app.use("/api/user", userRouter);
 app.use("/api/score", scoreRouter);
 app.use("/api/auth", authRouter);
-app.use("/api/question", questionRouter)
+app.use("/api/question", questionRouter);
 
 app.get("/", (req, res) => {
-    res.send("Hello world")
-})
+  res.send("Hello world");
+});
 
 app.use(errorHandler);
 app.use(routeNotFound);
 
 app.listen(PORT, () => {
-    console.log(`App is running on PORT ${PORT}`)
-})
+  console.log(`App is running on PORT ${PORT}`);
+});
